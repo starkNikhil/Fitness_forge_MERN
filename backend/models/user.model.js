15c235935cdefc6ba876074c6566043b6dbaf43f
Nishant Jain <jainnikhil0708@gmail.com>
2026-08-03 01:04:01 +0530

    feat: add user model and server setup with environment configuration

---
+++ backend/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userName : String,
+    email: String,
+    password: String
+
+})
+
+
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,42 @@ const express = require('express');
 
 
 
-const database = require('./config/database')
+// ENV configuration
+const dotenv = require('dotenv')
+dotenv.config()
+
+const connectDB = require('./config/database.js')
 
 const app = express();
 
 
+const PORT = process.env.PORT
 
 
+connectDB()
+
+
+// Middlewares
+app.use(express.json());
+// app.use()
+
+
+
+
+
+// routes
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT||3000,()=>{
+    console.log(`Server is running on http://localhost:${PORT}/`);
+    
+})
