6c78dfbc83f7533ce17451518c7d3b56565315d0
Nishant Jain <jainnikhil0708@gmail.com>
2026-08-22 15:11:14 +0530

    feat: refactor NavBar component structure and styling

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,9 +2,28 @@ import logo from "../assets/logo-img.png";
 
 function NavBar() {
   return (
-      <div style={{color:"blue", fontSize:"30px"}}>
-        Hey I am Nj.
+    <nav className="navbar flex justify-between ">
+      <div className="logo-section flex">
+        <a href="/">
+          <img src={logo} alt="Fitness-Forge" className="logo-image w-24"/>
+        </a>
+        <a href="">
+          <span>Fitness</span>
+          <span>Forge</span>
+        </a>
       </div>
+      <div className="navbar-toggle" id="mobile-menu">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <div className="navbar-menu navbar-animate flex justify-around
+       w-96">
+        <a href="" className="navbar-link">Home</a>
+        <a href="" className="navbar-link">About Us</a>
+        <a href="" className="navbar-link">Blogs</a>
+      </div>
+    </nav>
   );
 }
 
